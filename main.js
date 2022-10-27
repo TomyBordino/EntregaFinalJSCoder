17b0b3e7ff6f3723f9712a4f03afa19847101885
TomyBordino <tomybordino@hotmail.com>
2022-10-26 22:36:13 -0300

    Modificaciones

--- main.js
+++ main.js
@@ -1,153 +1,219 @@
-const db = {
-    methods: {
-        find: (id) => {
-            return db.items.find(item => item.id === id);
-        },
-        remove: (items) => {
-            items.array.forEach(element => {
-                const producto = db.methods.find(item.id);
-                producto.cantidad = producto.cantidad - item.cantidad;
-                
-            });
-            console.log(db);
-        },  
-    },
+let container = document.getElementById("container");
 
-    items : [ 
-        {
-            id:0,
-            titulo: 'Funko Pop',
-            precio: 5000,
-            cantidad: 10,
-
-        },
-        {
-            id:1,
-            titulo: 'Figura Luffy',
-            precio: 20000,
-            cantidad: 3,
-
-        },
-        {
-            id:2,
-            titulo: 'Manga One Piece',
-            precio: 900,
-            cantidad: 12,
-
-        },
-        {
-            id:3,
-            titulo: 'Manga Demon Slayer',
-            precio: 950,
-            cantidad: 8,
-
-        },
-    ]
+function Personaje(id, nombre, poder, vida, habilidadEsquivar, img) {
+	this.id = id;
+	this.nombre = nombre;
+	this.poder = poder;
+	this.vida = vida;
+	this.habilidadEsquivar = habilidadEsquivar;
+	this.img = img;
 }
 
+function random() {
+	let numeroRandom = Math.floor(Math.random() * 100);
+	return numeroRandom;
+}
 
-const carritoDeCompras = {
-    items: [],
-    methods: {
-        add: (id,cantidad) => {
-            const cartItem = carritoDeCompras.methods.get(id);
+function esquivar(Personaje) {
+	//Probabilidad de Esquivar
+	if (random() <= Personaje.habilidadEsquivar) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
-            if(cartItem){
-                if(carritoDeCompras.methods.inventario(id, cantidad + cartItem.cantidad)){
-                    
-                }else{
-                    alert("No hay inventario suficiente");
-                }
-                }else{
-                carritoDeCompras.items.push({ id, cantidad});
-                }
-            },
-            
+function atacar() {
+	//Cargar Daño
+	dañoAtaque = random() * 5;
+	return dañoAtaque;
+}
+
+let luffy = new Personaje(
+	1,
+	"LUFFY",
+	"Shotgun Gattlin",
+	1500,
+	30,
+	"./imagenes/luffyJS.jpg",
+);
+console.log(luffy.nombre);
+
+let goku = new Personaje(
+	2,
+	"GOKU",
+	"Kamehameha",
+	1700,
+	40,
+	"./imagenes/gokuJS.jpg",
+);
+console.log(goku.nombre);
+
+let vegeta = new Personaje(
+	3,
+	"VEGETA",
+	"Big Bang",
+	1700,
+	38,
+	"/imagenes/vegetaJS.jpg",
+);
+console.log(vegeta.nombre);
+
+let naruto = new Personaje(
+	4,
+	"NARUTO",
+	"Rasengan",
+	1000,
+	20,
+	"/imagenes/narutoJS.jpg",
+);
+console.log(naruto.nombre);
+
+let meliodas = new Personaje(
+	5,
+	"MELIODAS",
+	"Contra-Ataque Total",
+	1260,
+	32,
+	"/imagenes/meliodasJS.jpeg",
+);
+console.log(meliodas.nombre);
+
+let saitama = new Personaje(
+	6,
+	"SAITAMA",
+	"Golpe Simple",
+	1000,
+	50,
+	"/imagenes/saitamaJS.jpg",
+);
+console.log(saitama.nombre);
+
+let levi = new Personaje(
+	7,
+	"LEVI",
+	"Ataque Tridimensional",
+	800,
+	12,
+	"/imagenes/leviJS.jpg",
+);
+console.log(levi.nombre);
+
+let listaPersonajes = [luffy, goku, vegeta, naruto, meliodas, saitama, levi];
+
+alert("Toda la Informacion se mostrara por Consola  ");
+
+listaPersonajes.forEach((item) => {
+	const divCard = document.createElement("div");
+
+	const divContent = `
+    <div class = "card-body">
+    <h5 class = "card-title">${item.nombre}</h5>
+    <img id='${item.id}' class="imagenes" src = "${item.img}">
+    <p class = "card-text">${item.poder}</p>
+    <p class = "card-text">${item.vida}</p>
+    <p class = "card-text">${item.habilidadEsquivar}</p>
+    </div>
+    `;
+	divCard.innerHTML = divContent;
+	container.append(divCard);
+});
+
+
+
+
+function pelear(seleccion1, seleccion2){
+
+    
+
+    let i = 1
+    peleaActiva = true
+
+    while(peleaActiva != false){
+    
+        console.log("------------")
+        console.log("Ronda " + i)
         
-        remove: (id,cantidad) =>{
-            const cartItem = carritoDeCompras.methods.get(id);
-            if(cartItem.cantidad - cantidad > 0 ){
-                cartItem.cantidad -= cantidad;
+        i++
+        quienAtaca = random()
+
+        if(quienAtaca >= 51){
+            console.log(seleccion1.nombre + " Ataca a "+ seleccion2.nombre + " Con " + seleccion1.poder)
+            if(esquivar(seleccion2) === true){
+                //console.log(seleccion2.nombre + " Esquiva el Ataque")
+                const divCard = document.createElement("div");
+                const divContent = `
+                <div class = "card-body">
+                <img src = "${seleccion1.img}"> 
+                <h5 class = "card-text">Esquiva el ataque</p>
+                `
+                divCard.innerHTML = divContent;
+                container.append(divCard);
             }else{
-                carritoDeCompras.items = carritoDeCompras.items.filter(item => item.id != id);
+                dañoInflingido = atacar();
+                seleccion2.vida -= dañoInflingido;
+                console.log(seleccion2.nombre + " Recibe " + dañoInflingido + " Puntos de Daño " )
             }
-        },
-        count: () => {
-            return carritoDeCompras.items.reduce((acumulador, item) => acumulador+ item.cantidad , 0);
-        },
-        get: (id) =>{
-            const index = carritoDeCompras.items.findIndex((item) => item.id === id);
-            return index > 0 ? carritoDeCompras.items[index] : null;
-        },
-        getTotal: () => {
-            const total = carritoDeCompras.items.reduce((acumulador , item) => {
-                const found = db.methods.find(item.id);
-                return acumulador + found.precio * item.cantidad;
-            }, 0);
-            return total;
-        },
-        inventario: (id,cantidad) =>{
-            return db.items.find((item) => item.id === id).cantidad - cantidad >0;
-        },
-        comprar: () => {
-            db.methods.remove(carritoDeCompras.items);
-            carritoDeCompras.items = [];
-        },
+
+
+        }else{
+            console.log(seleccion2.nombre + " Ataca a "+ seleccion1.nombre + " Con " + seleccion2.poder)
+            if(esquivar(seleccion1) === true){
+                console.log(seleccion1.nombre + " Esquiva el Ataque")
+            }else{
+                dañoInflingido = atacar();
+                seleccion1.vida -= dañoInflingido;
+                console.log(seleccion1.nombre + " Recibe " + dañoInflingido + " Puntos de Daño " )
+            }
+        }
+
+        if(seleccion1.vida <= 0){
+            console.log("------------")
+            console.log(seleccion2.nombre + " Es el Ganador")
+            alert("Felicidades a " + seleccion2.nombre + "  Vencio a " + seleccion1.nombre)
+            peleaActiva = false;
+
+        }else if(seleccion2.vida <= 0){
+            console.log("------------")
+            console.log(seleccion1.nombre + " Es el Ganador")
+            alert("Felicidades a " + seleccion1.nombre + " Vencio a " + seleccion2.nombre)
+            peleaActiva = false;
+        }
+        console.log("*****")
+        console.log(seleccion1.nombre + " Tiene " + seleccion1.vida + " Puntos de vida")
+        console.log(seleccion2.nombre + " Tiene " + seleccion2.vida + " Puntos de vida")
 
     }
-};
-
-
-renderStore();
-
-
-//Creamos una Funcion
-function renderStore(){
-    const html = db.items.map((item ) => {
-        return `
-            <div class="item">
-                <div class="title">${item.titulo}</div>
-                <div class="price">${mostrarPrecio(item.precio)}</div>
-                <div class="cantidad">${item.cantidad}</div>
-
-                <div class = "actions">
-                    <button class = "add" data-id="${item.id
-                    }"> Añadir al Carrito</button>
-                </div>
-            </div>`;
-    });
-
-    document.querySelector("#store-container").innerHTML = html.join("");
-
-    document.querySelectorAll('.item .actions .add').forEach(button =>{
-        button.addEventListener('click' , e => {
-            const id = parseInt(button.getAttribute('data-id'));
-            const item = db.methods.find(id);
-
-            if(item && item.cantidad -1 > 0){
-                carritoDeCompras.methods.add(id,1);
-                console.log(carritoDeCompras);
-                renderCarritoDeCompras();
-
-            }
-            else{
-                console.log("Ya no Hay Inventario");
-            }
-        });
-    });
-};
-
-
-function renderCarritoDeCompras(){
-    
 }
 
-function mostrarPrecio(n){
-    //Utilizamos Una Api
-    return new Intl.NumberFormat('en-US',{
-        maximumSignificantDigits:2,
-        style: 'currency',
-        currency: 'USD'
-    }).format(n);
-}
+let seleccion1 = null
+let seleccion2 = null   
+
+
+const imagenes = document.querySelectorAll(".imagenes");
+imagenes.forEach((imagen) => {
+	imagen.addEventListener("click", (e) => {
+		const id = parseInt(e.target.id);
+		const personaje = listaPersonajes.find((personaje) => personaje.id === id);
+		//console.log(personaje);
+
+        if(seleccion1 === null){
+            seleccion1 = personaje;
+            console.log(seleccion1.nombre)
+        }else if (seleccion2 === null){
+            seleccion2 = personaje
+            console.log(seleccion2.nombre)
+            pelear(seleccion1,seleccion2)
+
+        }else{
+            console.log("Ya selecciono sus personajes")
+        }
+	});
+});
+
+
+const btn = document.createElement("button");
+btn.textContent = "sorpresa";
+
+console.log(listaPersonajes);
 
